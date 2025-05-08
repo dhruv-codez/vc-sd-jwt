337b6c9423e79d4f73e86a1fac5b9602f1650014
dhruv-codez <dhruv.prajapati@viitor.cloud>
2025-05-08 09:17:25 +0530

    Enhance key security with additional safeguard

--- src/utils/keyUtils.js
+++ src/utils/keyUtils.js
@@ -13,6 +13,23 @@ function ensureKeyDir() {
   if (!fs.existsSync(KEY_DIR)) {
     fs.mkdirSync(KEY_DIR, { recursive: true });
   }
+
+  // Ensure keys are in .gitignore
+  const gitignorePath = path.join(__dirname, '../../.gitignore');
+  if (fs.existsSync(gitignorePath)) {
+    const gitignoreContent = fs.readFileSync(gitignorePath, 'utf8');
+    if (!gitignoreContent.includes('/keys/')) {
+      console.warn('\x1b[33m%s\x1b[0m', 'WARNING: keys directory not found in .gitignore. This could lead to accidental commit of private keys!');
+      console.warn('\x1b[33m%s\x1b[0m', 'Please add "/keys/" to your .gitignore file.');
+    }
+  }
+
+  // Add a .gitignore file in the keys directory as an extra layer of protection
+  const keysGitignorePath = path.join(KEY_DIR, '.gitignore');
+  if (!fs.existsSync(keysGitignorePath)) {
+    fs.writeFileSync(keysGitignorePath, '# Ignore all files in this directory\n*\n!.gitignore\n');
+    console.log('Created .gitignore in keys directory for extra security');
+  }
 }
 
 /**
@@ -48,6 +65,7 @@ function generateKeyPair() {
   fs.writeFileSync(PRIVATE_KEY_PATH, privateKey);
   fs.writeFileSync(PUBLIC_KEY_PATH, publicKey);
   console.log('Keys generated and saved to', KEY_DIR);
+  console.log('\x1b[32m%s\x1b[0m', 'IMPORTANT: Never commit private keys to git repositories!');
 
   return { privateKey, publicKey };
 }
